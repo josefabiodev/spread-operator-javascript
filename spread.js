9443b1f966b6f3b920fbfcdd5c8d88695fc57662
josefabiodev <josefabiodev@gmail.com>
2023-10-26 10:07:13 -0300

    Spread Operator

---
+++ spread.js
@@ -0,0 +1,6 @@
+const notas = [7, 7, 8, 9];
+
+const novasNotas = [...notas, 10]; // novoArray copiado sem alterar o original
+
+console.log(`As novas notas são ${novasNotas}`);
+console.log(`As notas originais são ${notas}`);
